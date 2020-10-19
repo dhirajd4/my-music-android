cc889d517505dee7d0a7326e57618da589d03be9
Keshav Damani <42195243+KeshavGitHub1998@users.noreply.github.com>
2020-10-19 14:01:38 +0530

    updated testing

--- Music-player-master/app/src/androidTest/java/com/example/vivek/music/ExampleInstrumentedTest.java
+++ Music-player-master/app/src/androidTest/java/com/example/vivek/music/ExampleInstrumentedTest.java
@@ -20,6 +20,56 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
+        int main()
+{
+	boost
+	ll t;
+	cin>>t;
+	while(t--)
+	{
+		ll n;
+		cin>>n;
+		ll a[n];
+		for(ll i=0;i<n;i++)
+		{
+			cin>>a[i];
+		}
+		ll pos=0,neg=0,flag=-1;
+		ll sum=0;
+		for(ll i=0;i<n;i++)
+		{
+			if(a[i]>=0&&flag!=0)
+			{
+				pos=max(pos,a[i]);
+				sum+=neg;
+				neg=-inf;
+				flag=0;
+			}
+			else if(a[i]<0&&flag!=1)
+			{
+				if(neg==0)
+					neg=-inf;
+				neg=max(neg,a[i]);
+				sum+=pos;
+				pos=0;
+				flag=1;
+			}
+			else if(a[i]>0&&flag==0)
+			{
+				pos=max(pos,a[i]);
+			}
+			else if(a[i]<0&&flag==1)
+				neg=max(neg,a[i]);
+		}
+		if(flag==0&&neg!=-inf)
+			sum+=neg;
+		else
+			sum+=pos;
+		cout<<sum<<endl;
+	}
+
+	return 0;
+}
 
         assertEquals("com.example.vivek.music", appContext.getPackageName());
     }
